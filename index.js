8854a46c9082b14a05705077052cb4aa99911be2
enlore <n.e.lorenson@gmail.com>
2016-09-30 00:11:41 -0500

    Log a bit

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ const app = express()
 
 app.use(express.static(__dirname))
 
+app.use("/", (req, res, next) => {
+    console.info("got a request at " + req.url)
+    next()
+})
+
 app.listen(process.env.PORT)
