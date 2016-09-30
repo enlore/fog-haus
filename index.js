f0cf4f6576d59093bfe48a9e06ffd61fdd13e1e4
enlore <n.e.lorenson@gmail.com>
2016-09-30 00:13:20 -0500

    Log a bit more

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 const express = require("express")
 const app = express()
 
-app.use(express.static(__dirname))
-
 app.use("/", (req, res, next) => {
     console.info("got a request at " + req.url)
     next()
 })
 
-app.listen(process.env.PORT)
+app.use(express.static(__dirname))
+
+app.listen(process.env.PORT, () => {
+    console.info("Hey we're doin it")
+})
