e42fb37950b918ec842898a0176cc844260f6fc4
enlore <n.e.lorenson@gmail.com>
2016-11-14 10:41:53 -0600

    Build script

--- build.sh
+++ build.sh
@@ -1,11 +1,20 @@
 #! env bash
 echo "Build started"
-if [[ ! -d dist ]]; then
+if [[ -d dist ]]; then
+    echo "Deleteing dist dir"
+    rm -r dist
     mkdir dist
 fi
+
+echo "Copying over js, lib"
 cp -v src/*.js dist;
 cp -vr lib/* dist;
-stylus  --out dist src/main.styl;
-pug -O data.json --pretty --out dist src/index.pug;
+
+echo "Transpiling stylus"
+stylus --out dist src/styl/main.styl;
+
+echo "Transpiling pug"
+pug -O data.json --pretty --out dist src/pages/index.pug;
+
 echo "Build done"
 exit
