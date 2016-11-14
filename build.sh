01050d0d4e415523bba53f5c76557750547fc424
enlore <n.e.lorenson@gmail.com>
2016-11-14 10:51:37 -0600

    Use npm to run exec local bin

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #! env bash
 echo "Build started"
+
 if [[ -d dist ]]; then
     echo "Deleteing dist dir"
     rm -r dist
@@ -7,14 +8,14 @@ if [[ -d dist ]]; then
 fi
 
 echo "Copying over js, lib"
-cp -v src/*.js dist;
+cp -v src/js/*.js dist;
 cp -vr lib/* dist;
 
 echo "Transpiling stylus"
-stylus --out dist src/styl/main.styl;
+npm run styles:prod
 
 echo "Transpiling pug"
-pug -O data.json --pretty --out dist src/pages/index.pug;
+npm run html:prod
 
 echo "Build done"
 exit
