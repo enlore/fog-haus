7e7a01ca737fb454c38c71c18a07b5f79189e545
enlore <n.e.lorenson@gmail.com>
2017-01-15 17:11:45 -0600

    First

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+    #app
+        home
+
+</template>
+
+<script>
+    import Home from './components/home.vue'
+
+    export default {
+        name: 'app',
+
+        components: {
+            Home
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import 'normalize.css';
+    @import "../node_modules/susy/sass/susy";
+    @import 'style.scss';
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,146 @@
+<template lang="pug">
+    #home
+        .copyColumn
+            section
+                h1.thin.
+                    Business is changing. Sustainability and social innovation are at
+                    the forefront, and this new age of global megacorps is rapidly
+                    mutating.
+
+            section
+                h1.font-alt.alt.
+                    We like that.
+
+                p.
+                    At fog, we understand that the new consumer is more demanding and savvier than
+                    big companies are willing to admit. They want products with impact, that are
+                    forward thinking, and culturally-relevant. They're smart, capable, and worth
+                    knowing. They demand to be treated like humans.
+
+                p.
+                    And we know that demand will only intensify. Why? We’re just like you. We’re
+                    consumers and we’re business owners. We're demanding. We support socially
+                    conscious products. We understand that no one's going to build the future we
+                    want for us. We’ve helped companies and cool individuals launch, grow, and
+                    generate revenue while empowering their users to live better lives.
+
+                p.
+                    We started fog to do more of that.
+
+                p.
+                    Here’s how we do it.
+
+            section
+                .serviceBlock
+                    img.serviceIcon.u-imgResp.u-centerBlock(src="/dist/img/tail.svg")
+                    h2.alt.u-textCenter.
+                        Idea Validation
+
+                    p.
+                        Need an investor, or are you applying for an accelerator to push and fund your
+                        idea? We help you visualize your idea into a tangible design that refines your
+                        concept, establishes business need, and outfits you wireframes and art
+                        direction to help you make your pitch.
+
+                .serviceBlock
+                    img.serviceIcon.u-imgResp.u-centerBlock(src="/dist/img/uplift.svg")
+                    h2.alt.u-textCenter.
+                        Venture Development
+
+                    p.
+                        You’ve said to yourself, “This is it! I’ve got the idea that will radically
+                        change ____!” Hold up. Before you spend an exorbitant fee on getting your idea
+                        developed, think about the risk involved. Partner with us to build your MVP and
+                        reduce your initial overhead in exchange for an equity stake. Share the burden
+                        of launching a successful venture and gain access to a team of co-founders
+                        packed with nearly a decade of startup experience.
+
+
+                .serviceBlock
+                    img.serviceIcon.u-imgResp.u-centerBlock(src="/dist/img/wave.svg")
+                    h2.alt.u-textCenter.
+                        The Retainer
+
+                    p.
+                        When your needs are recurring (and, as they so often are, unpredictable), you
+                        need good people on call. You need design or development help ASAP? Problem
+                        solving? Firefighting? Consulting and training? We got you covered. As your
+                        partner, our flexible workflow helps bolster your internal team or gives you
+                        the extra kick you need when working with outsourced talent.
+
+                .serviceBlock
+                    img.serviceIcon.u-imgResp.u-centerBlock(src="/dist/img/responsive-sizes.svg")
+                    h2.alt.u-textCenter.
+                        Your Project
+
+                    p.
+                        “I need a website.” “I need branding.” “I need an app.” We’ve heard them all
+                        and we know how to help you. This is where we ask you a ton of questions,
+                        figure out your goals, define the scope, create & refine a concept, design the
+                        hell out of it, and develop it. Or any combination. You will be happy, and your
+                        users will thank you. You’re welcome.
+
+            section
+                h2.alt.
+                    You like that?  Get in touch.
+
+                p.
+                    Currently, we’re working on a few projects. If you like what you’ve been
+                    reading, well, you ought to shoot us a line and we'll get a conversation
+                    started.
+
+                h3.
+                    the fog team
+
+                ul
+                    li Nick Lorenson
+                    li Alexander Elliot
+                    li Ameer Suhayb Carter
+
+                p
+                    a(target="_blank", href="http://fog.haus/mailto:inquiries@fog.haus?subject=General%20Inquiries") Email us for questions ↗
+
+                p or
+
+                p
+                    a(target="_blank", href="http://fog.haus/mailto:inquiries@fog.haus?subject=Agency Services") agency services ↗
+
+                p or
+
+                p
+                    a.typeform-share(data-mode="1" target="_blank", href="https://nick669.typeform.com/to/uSyTW9") fill this out
+
+                p © 2016 fog.
+
+            section.easterEgg
+                h2.alt.
+                    By the way...
+
+                p.
+                    Ideas or no ideas, if you’re interested in journeying into entrepreneurship
+                    with digital products, we can provide a few things that might come in handy
+                    along the way.
+
+                h3.alt.
+                    Words
+
+                p.
+                    Thoughts on software, digital services, and the new wave of business.
+
+                p
+                    a(target="_blank", href="https://medium.com/portfolio-of-ameer-suhayb-carter/audible-channels-daed53a4ff07#.tdcval72n") Audible Channels - A Case Study ↗
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
+
